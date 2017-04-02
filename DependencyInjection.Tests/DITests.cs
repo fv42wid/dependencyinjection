219131db217fb9997dc59d0837e552fb54838440
Frank Vinci <fv42wid@yahoo.com>
2017-04-02 19:48:31 +0000

    complete chapter 18

---
+++ DependencyInjection.Tests/DITests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DependencyInjection.Controllers;
+using DependencyInjection.Models;
+using DependencyInjection.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace DependencyInjection.Tests
+{
+    public class DITests
+    {
+        [Fact]
+        public void ControllerTest()
+        {
+            //Arrange
+            var data = new[] { new Product { Name = "Test", Price = 100 } };
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Products).Returns(data);
+            //TypeBroker.SetTestObject(mock.Object);
+            HomeController controller = new HomeController(mock.Object);
+            //Act
+            ViewResult result = controller.Index();
+            //Assert
+            Assert.Equal(data, result.ViewData.Model);
+        }
+    }
+}
---
+++ DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DependencyInjection.Models;
+using DependencyInjection.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public IRepository Repository { get; } = TypeBroker.Repository;
+        //private IRepository repository;
+        //private ProductTotalizer totalizer;
+
+        /*public HomeController(IRepository repo)
+        {
+            repository = repo;
+            //totalizer = total;
+        }*/
+
+        public ViewResult Index([FromServices]ProductTotalizer totalizer)
+        {
+            IRepository repository = HttpContext.RequestServices.GetService<IRepository>();
+            ViewBag.HomeController = repository.ToString();
+            ViewBag.Totalizer = totalizer.Repository.ToString();
+            return View(repository.Products);
+        }
+    }
+}
